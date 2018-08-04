198066a840b5d6773e9c67baf5416e49c60ebc1c
quy <nguyenthequy.ptit@gmail.com>
2018-08-05 01:04:46 +0700

    Load Book by Sale

--- resources/views/sale/index.blade.php
+++ resources/views/sale/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.main_layout')
-@section('title', trans('messages.category'))
+@section('title', trans('messages.sale'))
 
 @section('content')
  <section id="content-holder" class="container-fluid container">
@@ -7,7 +7,7 @@
 		<section class="span9 first">
 			<div class="product_sort">
 			<div class="heading-bar">
-		  	<h2>{{ $category->name }}</h2>
+		  	<h2>{{ trans('messages.book_sale') }}</h2>
 		  		<span class="h-line"></span>
 		 	</div>
 				<div class="row-2">
@@ -23,15 +23,17 @@
 				</div>
 			</div>
 			<section class="grid-holder features-books">
-			@foreach($categories as $key => $cat)
+			@foreach($books_sale as $sale)
 				<figure class="span4 slide first ">
-					<a href="book-detail.html"><img class="image4" src="{{ $cat->image }}" alt="" class="pro-img"/></a>
-					<span class="title"><a href="book-detail.html">{{ $cat->name }}</a></span>
+					<a href="book-detail.html"><img class="image4" src="{{ $sale->image }}" alt="" class="pro-img"/></a>
+					<span class="title"><a href="book-detail.html">{{ $sale->name }}</a></span>
 					<span class="rating-bar"><img src="/images/rating-star.png" alt="Rating Star"/></span>
 					<div class="cart-price">
 						<a class="cart-btn2" href="cart.html">{{ trans('messages.add_cart') }}</a>
-						<span class="price">{{ number_format($cat->price) }} vnd</span>
+						<span class="price"><del class="price_sale">{{ number_format($sale->price) }} </del>  vnd</span>
+						<span class="price">{{ number_format(($sale->price * (100 - $sale->value))/100) }} vnd</span>
 					</div>
+					<span class="sale-icon">Sale</span>
 				</figure>
 			@endforeach
 			</section>
@@ -39,16 +41,16 @@
 			<div class="blog-footer">
 				<div class="pagination">  
 					<ul>
-						@if( $categories->currentPage() != 1 )
-						<li><a href="{{ $categories->url($categories->currentPage() - 1) }}">{{ trans('messages.prev') }}</a></li>
+						@if( $books_sale->currentPage() != 1 )
+						<li><a href="{{ $books_sale->url($books_sale->currentPage() - 1) }}">{{ trans('messages.prev') }}</a></li>
 						@endif
-						@for($i = 1 ; $i <= $categories->lastPage() ; $i++)
-						<li class="{{ ($categories->currentPage() == $i ) ? 'active' : '' }}">  
-							<a href="{{ $categories->url($i) }}">{{ $i }}</a>  
+						@for($i = 1 ; $i <= $books_sale->lastPage() ; $i++)
+						<li class="{{ ($books_sale->currentPage() == $i ) ? 'active' : '' }}">  
+							<a href="{{ $books_sale->url($i) }}">{{ $i }}</a>  
 						</li> 
 						@endfor
-						@if( $categories->currentPage() != $categories->lastPage() )
-						<li><a href="{{ $categories->url($categories->currentPage() + 1) }}">{{ trans('messages.next') }}</a></li>
+						@if( $books_sale->currentPage() != $books_sale->lastPage() )
+						<li><a href="{{ $books_sale->url($books_sale->currentPage() + 1) }}">{{ trans('messages.next') }}</a></li>
 						@endif
 					</ul>  
 				</div>
